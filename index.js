0dac87b82bf7978b94647e4681122e91afbf1c11
JunKyus <kuk941025@gmail.com>
2019-01-30 00:45:59 +0900

    added get

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 const routes = require('./routes/api');
 // set up express app
 const app = express();
 
+//connect to mongodb
+mongoose.connect('mongodb://localhost/ninjago');
+mongoose.Promise = global.Promise;
+
+app.use(bodyParser.json());
+
 app.use('/api', routes);
 
+//error handling
+app.use((err, req, res, next) => {
+    // console.log(err);
+    res.status(422).send({error: err._message});
+});
 //listen for request
 app.listen(process.env.port || 4000, () => {
     console.log('now listening for request');
--- routes/api.js
+++ routes/api.js
@@ -1,24 +1,34 @@
 const express = require('express');
 const router = express.Router();
-
+const Ninja = require('../models/ninja');
 //get a list from db
-router.get('/ninjas', (req,res) => {
-    res.send({type:'GET'});
+router.get('/ninjas', (req, res, next) => {
+    res.send({ type: 'GET' });
 });
 
 //add data to db
-router.post('/ninjas', (req,res) => {
-    res.send({type:'POST'});
+router.post('/ninjas', (req, res, next) => {
+    //create locally and save to db
+    Ninja.create(req.body).then((ninja) => {
+        res.send(ninja);
+    }).catch(next);
+
 });
 
 //update a ninja in db
-router.put('/ninjas/:id', (req,res) => {
-    res.send({type:'PUT'});
+router.put('/ninjas/:id', (req, res, next) => {
+    Ninja.findByIdAndUpdate({_id: req.params.id}, req.body).then(() => {
+        Ninja.findOne({_id:req.params.id}).then(ninja => {
+            res.send(ninja);
+        })
+    });
 });
 
 //delete from db
-router.delete('/ninjas/:id', (req,res) => {
-    res.send({type:'DELETE'});
+router.delete('/ninjas/:id', (req, res, next) => {
+    Ninja.findByIdAndDelete({_id: req.params.id}).then((ninja) => {
+        res.send(ninja);
+    });
 });
 
 module.exports = router;
